316932738f19ab09a4c10074564d64e39471d0ae
hamishow <163224379+hamishow@users.noreply.github.com>
2024-03-24 22:04:09 -0300

    Update Site.py

--- Site.py
+++ Site.py
@@ -34,46 +34,49 @@ def saida_insumo(nome, quantidade):
     c.execute('UPDATE insumos SET quantidade = quantidade - ? WHERE nome = ?', (quantidade, nome))
     conn.commit()
     conn.close()
+def visualizar_estoque():
+    conn = sqlite3.connect('estoque.db')
+    c = conn.cursor()
+    c.execute('SELECT nome, quantidade FROM insumos')
+    data = c.fetchall()
+    conn.close()
+
+    if not data:
+        st.warning('Não há insumos cadastrados.')
+    else:
+        st.write('### Quantidade de Insumos')
+        df = pd.DataFrame(data, columns=['Nome', 'Quantidade'])
+        df['Quantidade'] = df['Quantidade'].astype(str) + ' kg'  # Adiciona "kg" à quantidade
+        st.write(df)
+
 def main():
     st.title('Controle de Estoque')
 
-    opcao_cadastrar = st.button('Cadastrar Insumo')
-    opcao_entrada = st.button('Registrar Entrada')
-    opcao_saida = st.button('Registrar Saída')
-    opcao_visualizar = st.button('Visualizar Estoque')
+    operacao = st.sidebar.radio('Operação', [ 'Visualizar Estoque', 'Cadastrar Insumo', 'Registrar Entrada', 'Registrar Saída'])
 
-    if opcao_cadastrar:
+    if operacao == 'Cadastrar Insumo':
         nome = st.text_input('Nome do Insumo')
         quantidade = st.number_input('Quantidade', min_value=0.0, step=0.1)
         if st.button('Cadastrar'):
             cadastrar_insumo(nome, quantidade)
             st.success('Insumo cadastrado com sucesso!')
 
-    elif opcao_entrada:
+    elif operacao == 'Registrar Entrada':
         nome = st.selectbox('Insumo', obter_nomes_insumos())
         quantidade = st.number_input('Quantidade', min_value=0.0, step=0.1)
         if st.button('Registrar'):
             entrada_insumo(nome, quantidade)
             st.success('Entrada registrada com sucesso!')
 
-    elif opcao_saida:
+    elif operacao == 'Registrar Saída':
         nome = st.selectbox('Insumo', obter_nomes_insumos())
         quantidade = st.number_input('Quantidade', min_value=0.0, step=0.1)
         if st.button('Registrar'):
             saida_insumo(nome, quantidade)
             st.success('Saída registrada com sucesso!')
 
-    elif opcao_visualizar:
+    elif operacao == 'Visualizar Estoque':
         visualizar_estoque()
-
-def obter_nomes_insumos():
-    conn = sqlite3.connect('estoque.db')
-    c = conn.cursor()
-    c.execute('SELECT nome FROM insumos')
-    data = c.fetchall()
-    conn.close()
-    return [nome[0] for nome in data]
-
 
 
 if __name__ == '__main__':
